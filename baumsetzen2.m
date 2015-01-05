8a8824f46a97b16372b6d3c09dc9f95c4494a427
dkreuzer <david.kreuzer@uni-ulm.de>
2015-01-05 23:47:52 +0100

    Revert "fixed random points"
    This reverts commit f33fbfa1a5ea9df55494dfbe0508aaf8da027713.

--- baumsetzen2.m
+++ baumsetzen2.m
@@ -5,7 +5,7 @@ clear all
 x = [0, 2, 3, 1, 3, 1.5, 0];
 y = [0, 2, 1, -1, -1.5, 0.5, 2];
 g= graphics('roadtest1',5,x,y,0.001);
-%g.puttrees();
+g.puttrees();
 g.openWorld();
 g.placeNodes();
 g.movecar('Mercedes_S');
--- testRandomPoints.m
+++ testRandomPoints.m
@@ -1,15 +1,11 @@
 clc, clear;
 
 A = xlsread('VonUnibisRatio.xlsx','Tabelle3');
-x=A(50:60,1);
-y=A(50:60,2);
-
-x1 = [0, 2, 3, 1, 3, 1.5, 0]*5;
-y1 = [0, 2, 1, -1, -1.5, 0.5, 2]*5;
+x=A(1:end,1);
+y=A(1:end,2);
 
 
- s = street(x1,y1,0.001);
-[~, x, y, xr, yr, xl, yl] = s.getRouting(0.1);
-[a, b] = s.getRandomPoints(6, 0.1, 0.2);
+ s = street(x,y,0.001);
+[~, x, y, xr, yr, xl, yl] = s.getRouting(1000,1);
 %plot(x,y,'r',xr,yr,'g',xl,yl,'b')
-plot(x1, y1, 'O', x, y, a, b, 'O');
+%plot(x, y);
