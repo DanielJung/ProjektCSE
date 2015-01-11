5f0e079bfdbf59fc9cb987ecba803df2a4f11a80
Daniel <daniel.jung92@googlemail.com>
2015-01-11 15:39:23 +0100

    Added Timer-Function

--- Timer.m
+++ Timer.m
@@ -1,9 +1,25 @@
 classdef Timer
     
     properties
+        TimeInt     % Time-Interval to refresh
+        NextTime    % Next Time to refresh Sensors
+        Sensors     % Sensors to refresh
     end
     
     methods
+        function this = Timer(timeint, sensors)
+            this.TimeInt = timeint;
+            this.NextTime = timeint; % Next Time to refresh is first interval
+            this.Sensors = sensors;
+        end
+        function onUpdate(this, time)
+            for i=1:length(this.TimeInt)
+                if(this.NextTime(i)<=time)
+                    this.NextTime(i) = time+this.TimeInt(i);
+                    this.Sensors(i).onUpdate();
+                end
+            end
+        end
     end
     
 end
