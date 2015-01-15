1c6bda4babb2a1122480e2be559ad5cbad2b7bc4
Daniel <daniel.jung92@googlemail.com>
2015-01-15 13:04:10 +0100

    Added Car-functionality

--- Car.m
+++ Car.m
@@ -1,17 +1,53 @@
 classdef Car
     properties
-        timer
-        sensor1
-        sensor2
+        timer       % Timer für die Sensoren
+        street      % Referenz auf die Straße
+        time        % Aktuelle Simulationszeit
+        curSpeed    % Aktuelle Geschwindigkeit
+        speed       % Geschwindigkeitsmatrix
+        speedid     % Aktuelle ID in der Matrix
     end
     
     methods
-        function this=Car(dt)
+        function this=Car(rstreet, speed_file)
             this.timer = Timer();
-            this.sensor1 = Sensor1();
-            this.sensor2 = Sensor2();
+            this.street = rstreet;
+            this.time = 0;
+            this.curSpeed = 0;
+            this.speed = load(speed_file);
+            this.speedid = 1;
         end
         function this=onFrame(this)
+            this.street = this.street.step(this.curSpeed);
+            this.timer.onUpdate(this.time);
+            [m, n] = size(this.speed);
+            if(this.speedid<m)
+                if(this.speed(this.speedid+1, 1)>this.time) % Nächste Geschwindigkeit wird angenommen
+                    this.speedid = this.speedid+1;
+                    this.curSpeed = this.speed(this.speedid, 2);
+                end
+            end
+        end
+        function [s] = getStreet(this)
+            s = this.street;
+        end
+        function [x] = getPosition2D(this)
+            x = this.street.getPosition2D();
+        end
+        function [x] = getPosition3D(this)
+            x = this.street.getPosition3D();
+        end
+        function [r] = getRotation(this)
+            r = this.street.getRotation();
+        end
+        function [t] = getTime(this)
+            t = this.time;
+        end
+        function [r] = getWheelRotation(this)
+            r = this.street.getWheelRotation();
+        end
+        function [v] = getSpeed(this)
+            v = this.curSpeed;
         end
     end
     
---
+++ Street.m
@@ -0,0 +1,182 @@
+classdef Street < handle
+    
+    properties (GetAccess=public)
+        CoeffMatX   % Coefficient-Matrix for X-Axis
+        dCoeffMatX  % Coefficient-Matrix for derivate X
+        CoeffMatY   % Coefficient-Matrix for Y-Axis
+        dCoeffMatY  % Coefficient-Matrix for derivate Y
+        Time        % Time-Interval
+        DeltaT      % Time-Step
+        NumSteps    % Number of Steps
+        WheelRot    % Rotationvom Rad
+        CurTime     % Current Time
+        CurPos      % Current Position
+        Rotation    % Current Rotation
+        frame       % an welcher Stelle der Stra�e befindet man sich (Nummer der koordinate)
+    end
+    
+    methods
+        function this=Street(x, y, dt)
+            this.Time           = 0:length(x)-1;
+            this.DeltaT         = dt;
+            this.CurTime        = 0;
+            this.NumSteps       = (length(this.Time)-1)/this.DeltaT;
+            this.CurPos(1)      = x(1);
+            this.CurPos(2)      = y(1);
+            this.frame          = 1;
+            this.CoeffMatX      = getSplineCoefficients(this.Time, x, 0, 0);
+            this.CoeffMatY      = getSplineCoefficients(this.Time, y, 0, 0);
+            this.WheelRot       = 0;
+            this.dCoeffMatX     = diffCoeffMat(this.CoeffMatX);
+            this.dCoeffMatY     = diffCoeffMat(this.CoeffMatY);
+        end
+        function this=step(this, v)
+            dx = evalSpline(this.dCoeffMatX, this.Time, this.CurTime);
+            dy = evalSpline(this.dCoeffMatY, this.Time, this.CurTime);
+            abs = norm([dx, dy]);
+            
+            this.CurTime = this.CurTime + this.DeltaT*v/abs;
+            
+            this.CurPos(1) = this.CurPos(1)+v*this.DeltaT*dx/abs;
+            this.CurPos(2) = this.CurPos(2)+v*this.DeltaT*dy/abs;
+            this.Rotation = atan2(dy, dx)-pi;
+            this.frame  = this.frame+v;
+            this.RadRotation = this.RadRotation +2*v ;
+            if  (this.WheelRot >= 360)
+                this.WheelRot = this.WheelRot-360;
+            end
+            
+        end
+        function [t, x, y, xr, yr, xl, yl] = getRouting(this,  width)
+            t = linspace(0, max(this.Time), this.NumSteps);
+            x = zeros(this.NumSteps, 1);
+            xr = zeros(this.NumSteps, 1);
+            xl = zeros(this.NumSteps, 1);
+            y = zeros(this.NumSteps, 1);
+            yr = zeros(this.NumSteps, 1);
+            yl = zeros(this.NumSteps, 1);
+            alpha = zeros(this.NumSteps,1);
+            for i=1:length(t)
+                x(i, 1) = evalSpline(this.CoeffMatX, this.Time, t(i));
+                y(i, 1) = evalSpline(this.CoeffMatY, this.Time, t(i));
+                
+                dx = evalSpline(this.dCoeffMatX, this.Time, t(i));
+                dy = evalSpline(this.dCoeffMatY, this.Time, t(i));
+                %Winkel berenchen
+                xy(1, 1) = x(i);
+                xy(2, 1) = y(i);
+                xy = xy/norm(xy);
+                e_y=[0 1];
+                
+                %Berechne rechten Rand
+                xy = [0;0];
+                xy(1, 1) = dy;
+                xy(2, 1) = -dx;
+                xy = xy/norm(xy)*width/2;
+                
+                xr(i, 1) = x(i, 1)+xy(1, 1);
+                yr(i, 1) = y(i, 1)+xy(2, 1);
+                
+                %Berechne linken Rand
+                xy(1, 1) = -dy;
+                xy(2, 1) = dx;
+                xy = xy/norm(xy)*width/2;
+                
+                xl(i, 1) = x(i, 1)+xy(1, 1);
+                yl(i, 1) = y(i, 1)+xy(2, 1);
+            end
+        end
+        function [x] = getPosition2D(this)
+            x = [this.CurPos(1), this.CurPos(2)];
+        end
+        function [x] = getPosition3D(this)
+            x = [this.CurPos(1), 0.2590, this.CurPos(2)];
+        end
+        function [x, y] = getRightLimit(this)
+            tx = evalSpline(this.CoeffMatX, this.Time, this.CurTime);  % Position auf der Stra�e
+            ty = evalSpline(this.CoeffMatY, this.Time, this.CurTime);
+
+            dx = evalSpline(this.dCoeffMatX, this.Time, this.CurTime);  % Ableitung an der Stelle
+            dy = evalSpline(this.dCoeffMatY, this.Time, this.CurTime);
+
+            %Berechne rechten Rand
+            xy = [0;0];
+            xy(1, 1) = dy;
+            xy(2, 1) = -dx;
+            xy = xy/norm(xy);
+
+            x = tx+xy(1, 1);
+            y = ty+xy(2, 1);
+        end
+        function [a] = getRotation(this)
+            a = this.Rotation;
+        end
+        function [radr] = getWheelRotation(this)
+            radr = this.WheelRot;
+        end
+        function [b] = getFrame(this)
+                b = this.frame;
+        end
+        function [dx, dy] = getDerivative(this, numSteps)
+            t = linspace(0, max(this.Time), numSteps);
+            dx = zeros(numSteps, 1);
+            dy = zeros(numSteps, 1);
+            for i=1:length(t)
+                dx(i) = evalSpline(this.dCoeffMatX, this.Time, t(i));
+                dy(i) = evalSpline(this.dCoeffMatY, this.Time, t(i));
+            end
+        end
+        function [x, y] = getRandomPoints(this, n, dist, width)
+            x = zeros(n, 1);
+            y = zeros(n, 1);
+            for i=1:n/2  % B�ume rechts der Stra�e
+                t = rand()*this.Time(end); % Zuf�lliger Zeitpunkt
+                
+                tx = evalSpline(this.CoeffMatX, this.Time, t);  % Position auf der Stra�e
+                ty = evalSpline(this.CoeffMatY, this.Time, t);
+                
+                dx = evalSpline(this.dCoeffMatX, this.Time, t);  % Ableitung an der Stelle
+                dy = evalSpline(this.dCoeffMatY, this.Time, t);
+                
+                %Berechne rechten Rand
+                xy = [0;0];
+                xy(1, 1) = dy;
+                xy(2, 1) = -dx;
+                xy = xy/norm(xy);
+                
+                x(i) = tx+xy(1, 1)*(dist+rand()*width/2);
+                y(i) = ty+xy(2, 1)*(dist+rand()*width/2);
+            end
+            for i=(n/2+1):n % B�ume links der Stra�e
+                t = rand()*this.Time(end); % Zuf�lliger Zeitpunkt
+                
+                tx = evalSpline(this.CoeffMatX, this.Time, t);  % Position auf der Stra�e
+                ty = evalSpline(this.CoeffMatY, this.Time, t);
+                
+                dx = evalSpline(this.dCoeffMatX, this.Time, t);  % Ableitung an der Stelle
+                dy = evalSpline(this.dCoeffMatY, this.Time, t);
+                
+                %Berechne linken Rand
+                xy = [0;0];
+                xy(1, 1) = -dy;
+                xy(2, 1) = dx;
+                xy = xy/norm(xy);
+                
+                x(i) = tx+xy(1, 1)*(dist+rand()*width/2);
+                y(i) = ty+xy(2, 1)*(dist+rand()*width/2);
+            end
+        end
+        function [x, y] = filterPoints(this, ox, oy, minDist)
+            x = ox(1);
+            y = oy(1);
+            
+            for i=2:length(ox)
+                if sqrt((ox(i)-x(end))^2+(oy(i)-y(end))^2)>minDist % Abstand gr��er als Mindestabstand => �bernehme Koordinate
+                    x = [x, ox(i)];
+                    y = [y, oy(i)];
+                end
+            end
+        end
+    
+end
+end
